3f16a2e4788700a7ea3e50469fc122f296f9cf05
bepip <beri073@hotmail.fr>
2024-03-11 01:32:57 +0100

    main.py changed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
-from textnode import TextNode
-from htmlnode import HTMLNode
+from textnode import (
+      TextNode, 
+      text_type_bold,
+      text_type_code,
+      text_type_image,
+      text_type_italic,
+      text_type_link,
+      text_type_text
+      )
+from htmlnode import HTMLNode, LeafNode
 
 def main():
       a = TextNode("This is a text node", "bold", "https://www.boot.dev")
       b = TextNode("This is a text node", "bold", "https:/s/www.boot.dev")
       node2 = HTMLNode("h1","This is a header", [], {"href": "https://www.google.com", "target": "_blank"})
+      leaf = LeafNode("h1","This is a header")
       print(a) 
       print(b) 
       print(node2)
+      print(leaf.to_html())
 main()
